e0f7bf53131593017860434d7fa960a01a6d3ff8
Nursik <tashanov_nb@smart-transport.ru>
2021-02-08 11:53:08 +0500

    ...

---
+++ build/precache-manifest.017106b0e704cd9625dc0f0505491d60.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f0ec54715744e7b2bb169f04f19dad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2bbe9fc39f0607861cd",
+    "url": "/static/css/main.8e071235.chunk.css"
+  },
+  {
+    "revision": "c84dac4938b1522c8799",
+    "url": "/static/js/2.8765814b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8765814b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bbe9fc39f0607861cd",
+    "url": "/static/js/main.68d5c0f3.chunk.js"
+  },
+  {
+    "revision": "e6b2f08932af1432b701",
+    "url": "/static/js/runtime-main.22415690.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/static/media/pizza-logo.f3327011.svg"
+  }
+]);
